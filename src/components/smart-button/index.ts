990fca57b66b87fd0a8681375e6d1ec00bf56b83
Vivek Singh <viveksingh0143@gmail.com>
2024-12-12 18:49:48 +0530

    Smart Button Deleted and package.json updated

--- src/components/smart-button/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './button';
-export * from './types';
-export * from './constants';
--- src/components/smart-button/types.ts
+++
@@ -1,8 +0,0 @@
-export type SmartButtonVariant = 'primary' | 'secondary' | 'outline';
-export type SmartButtonSize = 'small' | 'medium' | 'large';
-
-export interface SmartButtonProps {
-  variant: SmartButtonVariant;
-  size: SmartButtonSize;
-  disabled: boolean;
-}
